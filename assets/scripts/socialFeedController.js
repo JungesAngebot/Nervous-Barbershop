0f79ac7c32cdd8106cd6ecd9d1db0f95f83ecb79
Vadim Bauer <vadim.bauer@gmail.com>
2016-12-12 17:58:27 +0100

    Finaler Kram

--- assets/scripts/socialFeedController.js
+++ assets/scripts/socialFeedController.js
@@ -9,9 +9,9 @@ var socialFeedController = function () {
         $('#feedLinks').socialfeed({
             // FACEBOOK
             facebook:{
-                accounts: ['@linkspartei','@AntifaschistischesInfoblatt','@neues eutschland','@junge.welt','@JungleWorld','@sahra.wagenknecht','@Graswurzel.net'],
+                accounts: ['@183921262475','@derfunke.de','@linkspartei','@AntifaschistischesInfoblatt','@Rote-Flora','@neuesdeutschland','@junge.welt','@JungleWorld','@sahra.wagenknecht','@Graswurzel.net', '@anarchistischesnetzwerksuedwest'],
                 limit: 2,
-                access_token: ''
+                access_token: '652139488281223|6ab1dd4d9b3947ee7fc44dbd6dda36a2'
             },
             //  GENERAL SETTINGS
             length: 200,
@@ -31,9 +31,9 @@ var socialFeedController = function () {
         $('#feedMitteLinks').socialfeed({
             // FACEBOOK
             facebook:{
-                accounts: ['@SPD','@zeitonline','@Zett.home','@netzpolitik','@taz.kommune','@B90DieGruenen','@correctiv.org','@bund.bundesverband'],
+                accounts: ['@SPD','@zeitonline','@Zett.home','@tagesspiegel','@netzpolitik','@taz.kommune','@B90DieGruenen','@correctiv.org','@bund.bundesverband','@kban.Kampagne','@ihre.sz'],
                 limit: 2,
-                access_token: ''
+                access_token: '652139488281223|6ab1dd4d9b3947ee7fc44dbd6dda36a2'
             },
             //  GENERAL SETTINGS
             length: 200,
@@ -53,9 +53,9 @@ var socialFeedController = function () {
         $('#feedMitteRechts').socialfeed({
             // FACEBOOK
             facebook:{
-                accounts: ['@faz','@CiceroMagazin','@Bild','@focus.de','@Huffingtonpostde','@CDU','@StuttgarterZeitung','@bundesjugendring','@jugend.im.dgb'],
+                accounts: ['@faz','@CiceroMagazin','@Bild','@welt','@focus.de','@Huffingtonpostde','@CDU','@StuttgarterZeitung','@bundesjugendring','@abendblatt','@rponline','@tichyseinblick'],
                 limit: 2,
-                access_token: ''
+                access_token: '652139488281223|6ab1dd4d9b3947ee7fc44dbd6dda36a2'
             },
             //  GENERAL SETTINGS
             length: 200,
@@ -75,9 +75,9 @@ var socialFeedController = function () {
         $('#feedRechts').socialfeed({
             // FACEBOOK
             facebook:{
-                accounts: ['@Alternativefuerde','@jungefreiheit','@identitaere','@Dr.Frauke.Petry','@BeatrixVonStorch','@Luegenpress','@ferieninsyrien','@fuer.die.heimat'],
+                accounts: ['@Alternativefuerde','@jungefreiheit','@npd.de','@identitaere','@Dr.Frauke.Petry','@BeatrixVonStorch','@Luegenpress','@fuer.die.heimat','@legida.eu','@politikversagen','@contraonlinemagazin','@Compact.Magazin'],
                 limit: 2,
-                access_token: ''
+                access_token: '652139488281223|6ab1dd4d9b3947ee7fc44dbd6dda36a2'
             },
             //  GENERAL SETTINGS
             length: 200,
